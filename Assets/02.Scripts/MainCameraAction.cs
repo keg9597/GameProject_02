b0f195cb14963906b2a9327487824af7896d2dc2
keg9597 <98301838+keg9597@users.noreply.github.com>
2023-04-18 18:13:57 +0900

    카메라 이동
    카메라 이동 수정해야됨

--- Assets/02.Scripts/MainCameraAction.cs
+++ Assets/02.Scripts/MainCameraAction.cs
@@ -4,28 +4,24 @@ using UnityEngine;
 
 public class MainCameraAction : MonoBehaviour
 {
-    public GameObject Target; //카메라가 따라다닐 타겟 
+    public float sensitivity = 100f;
+    public Transform player;
+    float xRotation = 0f;
 
-    public float offsetX = 0.0f; //카메라의 좌표 X
-    public float offsetY = 10.0f; //카메라의 좌표 Y
-    public float offsetZ = -10.0f; //카메라의 좌표 Z
-
-    public float angleX = 0.0f; //카메라 앵글 X
-    public float angleY = 0.0f; //카메라 앵글 Y
-    public float angleZ = 0.0f; //카메라 앵글 Z
-
-    public float cameraSpeed = 10.0f; //카메라의 속도
-    Vector3 TargetPos; //타겟의 위치
-
-    private void FixedUpdate()
+    private void Start()
     {
-        //타겟의 x,y,z 좌표에 카메라 좌표를 더하여 카메라의 위치를 결정
-        TargetPos = new Vector3(
-            Target.transform.position.x + offsetX,
-            Target.transform.position.y + offsetY,
-            Target.transform.position.z + offsetZ);
-        //카메라의 움직임을 부드럽게 하는 함수Lerp
-        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime);
-        transform.rotation = Quaternion.Euler(angleX, angleY, angleZ);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
     }
 }
