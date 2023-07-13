f079efe1ed556ebf7691ed317b81ee9da628235a
keg9597 <98301838+keg9597@users.noreply.github.com>
2023-07-13 17:01:53 +0900

    수정
    수정

---
+++ Assets/02.Scripts/FollowCam.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform targetTr;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/02.Scripts/PlayerController.cs
+++ Assets/02.Scripts/PlayerController.cs
@@ -5,85 +5,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private Animator picoChanAnim;
-    public float speed = 10;
+    public float moveSpeed = 10;
     public float jumpHeight = 3f; //점프 높이 설정
     public bool isGrounded; //땅에 서있는지 체크하기 위한 bool값
     public LayerMask ground; //레이어 마스크 설정
     public float groundDistance = 0.2f;
-
     public float attackCulTime;
-    public float attackCoolTime;   
+    public float attackCoolTime;
 
+    private Animator picoChanAnim;
     private Rigidbody rb;
     Vector3 dir = Vector3.zero;
         
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.drag = 1;
+        //rb.drag = 1;
         picoChanAnim = this.gameObject.transform.GetChild(0).GetComponent<Animator>();
     }
 
     void Update()
-    {
-        InputAndDir();
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            attackCulTime += Time.deltaTime;
-            if(attackCulTime > attackCoolTime)
-            {
-                attackCulTime = 0;
-                Debug.Log("공격");
-            }
-        }
-
-        //if(picoChanAnim != null)
-        //{
-        //    picoChanAnim.SetInteger("playerState", 1);
-        //    switch (playerState)
-        //    {
-        //        case PLAYERSTATE.IDLE:
-        //            picoChanAnim.SetInteger("STATE", 0);
-        //            transform.position = Vector3.zero;
-        //            GetComponent<BoxCollider>().size = new Vector3(1f, 2.8f, 1f);
-        //            GetComponent<BoxCollider>().center = new Vector3(0, 0.9f, 0);
-        //            jumpHeight = 3f;
-        //            if (Input.GetButtonDown("Jump"))
-        //            {
-        //                playerState = PLAYERSTATE.JUMP;
-        //            }
-        //            break;
-        //        case PLAYERSTATE.RUN:
-        //            picoChanAnim.SetInteger("STATE", 1);
-        //            Debug.Log(1);
-        //            transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        //            if(Input.GetButtonDown("Jump"))
-        //            {
-        //                playerState = PLAYERSTATE.JUMP;
-        //            }
-        //            break;
-        //        case PLAYERSTATE.JUMP:
-        //            break;
-        //        case PLAYERSTATE.ATTACK:
-        //            break;
-        //        default:
-        //            break;
-        //    }
-        //}
-        
+    {      
         GroundCheck(); //땅 위에 서있는
-        
+        Move();
         Jump();
+        Atttack();
+        Turn();
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + dir * speed * Time.deltaTime);
+        rb.MovePosition(rb.position + dir * moveSpeed * Time.deltaTime);
     }
 
-    void InputAndDir()
+    void Move()
     {
         Vector3 moveVec = new Vector3(dir.x, 0, dir.z);
         float move = moveVec.magnitude;
@@ -92,18 +47,49 @@ public class PlayerController : MonoBehaviour
      
         dir.x = Input.GetAxis("Horizontal");
         dir.z = Input.GetAxis("Vertical");
-        if(dir != Vector3.zero)
+
+        Vector3 Position = transform.position;
+
+        Position.x += dir.x * Time.deltaTime * moveSpeed;
+        Position.z += dir.z * Time.deltaTime * moveSpeed;
+
+        transform.position = Position;
+
+        if (dir != Vector3.zero)
         {
             transform.forward = dir;
         }
     }
 
+    void Turn()
+    {
+        if(dir.x == 0 && dir.z == 0)
+            return;
+
+        Quaternion newRotation = Quaternion.LookRotation(dir);
+        rb.rotation = Quaternion.Slerp(rb.rotation, newRotation, moveSpeed * Time.deltaTime);
+    }
+
     void Jump()
     {
+        //picoChanAnim.SetTrigger("Jump");
         if (Input.GetButtonDown("Jump") && isGrounded == true)	// IsGrounded가 true일 때만 점프할 수 있도록
         {
             Vector3 jumpVelocity = Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
             rb.AddForce(jumpVelocity, ForceMode.VelocityChange);
+        }
+    }
+
+    void Atttack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            attackCulTime += Time.deltaTime;
+            if (attackCulTime > attackCoolTime)
+            {
+                attackCulTime = 0;
+                Debug.Log("공격");
+            }
         }
     }
 
